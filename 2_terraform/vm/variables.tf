1191e0886bebc4a03cda19fe373f25f4c7c20231
chrisvugrinec <chrisvugrinec@gmail.com>
2019-07-05 12:12:05 +1000

    added vm

---
+++ 2_terraform/vm/variables.tf
@@ -0,0 +1,13 @@
+variable "vm_size" {
+  default = "Standard_DS3_v2"
+}
+variable "image-rg" {
+  default = "hashi-demo-images"
+}
+variable "ssh" {}
+variable "static_ip" {}
+variable "hostname" {}
+variable "rg" {}
+variable "vnet" {}
+variable "subnet" {}
+variable "keyvault" {}
